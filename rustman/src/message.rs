c56d108f8300a14312b146a40376c8031cb54299
Lyes Djaoui <lyesdjaoui.pro@gmail.com>
2026-02-07 18:31:21 +0100

    message type

---
+++ rustman/src/message.rs
@@ -0,0 +1,22 @@
+use crate::http_client::{ApiResponse, HttpMethod};
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ResponseTab {
+    Body,
+    Headers,
+}
+
+#[derive(Debug, Clone)]
+pub enum Message {
+    UrlChanged(String),
+    MethodSelected(HttpMethod),
+    HeaderKeyChanged(usize, String),
+    HeaderValueChanged(usize, String),
+    AddHeader,
+    RemoveHeader(usize),
+    BodyChanged(String),
+    SendRequest,
+    RequestCompleted(Result<ApiResponse, String>),
+    TabChanged(ResponseTab),
+}
