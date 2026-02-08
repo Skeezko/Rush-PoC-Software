7c2191b767314c487a7b0dd524e8a7979e7870e5
Skeezko <heshamch2005@gmail.com>
2026-02-08 01:38:51 +0100

    fix: http_clients.rs

--- rustman/src/http_client.rs
+++ rustman/src/http_client.rs
@@ -23,11 +23,7 @@ impl HttpMethod {
     }
 }
 
-<<<<<<< HEAD
 #[derive(Debug, Clone)]
-=======
-#[derive(Clone,Debug)]
->>>>>>> refs/remotes/origin/main
 pub struct ApiResponse {
     pub status: u16,
     pub status_text : String,
